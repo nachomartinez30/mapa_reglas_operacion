a8da881961d7ff743b8eef4d1f011bbc15959431
Jorge <akiubarx@gmail.com>
2019-03-01 12:10:26 -0600

    Inicio de Proyecto

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Reglas de Operacion 2019</title>
+
+
+    <!-- CSS -->
+    <link href="/favicon.ico" rel="shortcut icon">
+    <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Respond.js soporte de media queries para Internet Explorer 8 -->
+    <!-- ie8.js EventTarget para cada nodo en Internet Explorer 8 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+    <!-- Contenido -->
+    <main class="page">
+      <div class="container-fluid">
+      	<div class="row">
+      		<div class="col-md-10 col-lg-10 center-element clear-both">
+      			<h2>Mapa RO 2019</h2>
+      		</div>
+      		<div class="col-md-8 col-lg-8 center-element clear-both">
+      			<img src="img/mapa.jpg" class="img-responsive" alt="">
+      		</div>
+      	</div>
+      </div>
+    </main>
+
+    <!-- JS -->
+    <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+
+  </body>
+</html>
